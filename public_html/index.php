670a6c40dfbfe0bf447a3a1a8e6b34eb48d81d93
Nick Srsen <nsrsen18@gmail.com>
2018-03-27 14:54:05 -0500

    picture inserted

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>M&N Paints</title>
+<title>M & N Paints</title>
 </head>
 <link rel="stylesheet" type="text/css" href="style.css"/>
 <body>
-  <h1>M&N Paints</h1>
+  <h1>M & N Paints</h1>
     <p>Welcome to M&N Paints. We have the best granite paints and the best prices in the country.</p>
-
+    <img src="http://foothillsrunning.com/cool-granite-countertops-types/countertop-granite-countertops-types-marble-colors-of-granitea-105/"
+ alt="Color Options"/>
 
 
 
